877ca233fd105564d99fd8eaae808b88c2e011ef
Andre Honorio <andresilva@setic.ro.gov.br>
2021-10-19 22:31:35 -0400

    testando implementação

---
+++ public/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <title></title>
+    </head>
+    <body>
+        <div class="container mt-4">
+            <form action="" method="post">
+                <div class="mb-3">
+                    <input class="form-control" type="text" name="nome" placeholder="nome">
+                </div>
+                <div class="mb-3">
+                    <input class="form-control" type="email" name="email" placeholder="email@email.com">
+                </div>
+                <div class="mb-3">
+                    <textarea class="form-control" name="descricao" cols="30" rows="10" placeholder="Descrição"></textarea>
+                </div>
+                <button class="btn btn-primary" type="submit">Enviar</button>
+            </form>
+        </div>
+    </body>
+</html>
